9f3d3c87bb38499ecb5008b704ec9a14f574f53d
Rohit Bagal <bagalrohit75@gmail.com>
2023-06-10 23:12:36 +0530

    main

--- src/main.js
+++ src/main.js
@@ -1,2 +1,13 @@
+import { MongoClient } from "mongodb";
 
-console.log("Hello");
+async function main(){
+    const uri = "mongodb://127.0.0.1:27017";
+    const client = new MongoClient(uri);
+    const db = client.db("mydb");
+    const message = db.collection("message");
+await message.insertOne({});
+await client.close();
+  console.log("Record Added");
+}
+
+main();

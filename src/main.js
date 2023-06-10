680b4e048501a7c6ed5f1cc3349ec67862633672
Rohit Bagal <bagalrohit75@gmail.com>
2023-06-10 23:27:19 +0530

    branch 2, seperate insert method.

--- src/main.js
+++ src/main.js
@@ -1,13 +1,18 @@
 import { MongoClient } from "mongodb";
 
-async function main(){
+async function insertRecord(){
     const uri = "mongodb://127.0.0.1:27017";
     const client = new MongoClient(uri);
     const db = client.db("mydb");
     const message = db.collection("message");
-await message.insertOne({});
+let jsonDocument = {message:"Hello World",to: "rohit",from:"Pramod"};
+await message.insertOne(jsonDocument);
+//close connection
+
 await client.close();
   console.log("Record Added");
 }
-
+async function main(){
+await  insertRecord();
+}
 main();
